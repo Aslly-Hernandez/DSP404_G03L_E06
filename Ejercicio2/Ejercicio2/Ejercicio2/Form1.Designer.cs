4f71721decb68b9202260088be4440d79f46cfcd
Aslly-Hernandez <Aslly.hernandz@gmail.com>
2024-08-11 00:22:53 -0600

    Agrege funcionalidad de la entrada de la cuenta

--- Ejercicio2/Ejercicio2/Ejercicio2/Form1.Designer.cs
+++ Ejercicio2/Ejercicio2/Ejercicio2/Form1.Designer.cs
@@ -28,13 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblMensaje = new System.Windows.Forms.Label();
+            this.txtNumeroCuenta = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnConfirmarCuenta = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // lblMensaje
+            // 
+            this.lblMensaje.AutoSize = true;
+            this.lblMensaje.Location = new System.Drawing.Point(103, 290);
+            this.lblMensaje.Name = "lblMensaje";
+            this.lblMensaje.Size = new System.Drawing.Size(44, 16);
+            this.lblMensaje.TabIndex = 2;
+            this.lblMensaje.Text = "label3";
+            // 
+            // txtNumeroCuenta
+            // 
+            this.txtNumeroCuenta.Location = new System.Drawing.Point(218, 66);
+            this.txtNumeroCuenta.Name = "txtNumeroCuenta";
+            this.txtNumeroCuenta.Size = new System.Drawing.Size(100, 22);
+            this.txtNumeroCuenta.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(218, 174);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(445, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnConfirmarCuenta
+            // 
+            this.btnConfirmarCuenta.Location = new System.Drawing.Point(445, 146);
+            this.btnConfirmarCuenta.Name = "btnConfirmarCuenta";
+            this.btnConfirmarCuenta.Size = new System.Drawing.Size(146, 50);
+            this.btnConfirmarCuenta.TabIndex = 6;
+            this.btnConfirmarCuenta.Text = "Confirmar Cuenta";
+            this.btnConfirmarCuenta.UseVisualStyleBackColor = true;
+            this.btnConfirmarCuenta.Click += new System.EventHandler(this.btnConfirmarCuenta_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(445, 229);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(218, 290);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnConfirmarCuenta);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtNumeroCuenta);
+            this.Controls.Add(this.lblMensaje);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblMensaje;
+        private System.Windows.Forms.TextBox txtNumeroCuenta;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnConfirmarCuenta;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- Ejercicio2/Ejercicio2/Ejercicio2/Form1.cs
+++ Ejercicio2/Ejercicio2/Ejercicio2/Form1.cs
@@ -12,9 +12,37 @@ namespace Ejercicio2
 {
     public partial class Form1 : Form
     {
+        private Dictionary<int, decimal> cuentas = new Dictionary<int, decimal>()
+    {
+        { 1000001, 400m },
+        { 1000002, 200m },
+        { 1000004, 500m }
+    };
+
+        private int cuentaActual;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConfirmarCuenta_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtNumeroCuenta.Text, out cuentaActual) && cuentas.ContainsKey(cuentaActual))
+            {
+                lblMensaje.Text = $"Bienvenido, cuenta {cuentaActual}";
+                MostrarMenu();
+            }
+            else
+            {
+                lblMensaje.Text = "Número de cuenta no válido. Inténtelo de nuevo.";
+            }
         }
     }
 }
