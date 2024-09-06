971c2e5cc17fb01fa6ffd9c712136bc95dd96cd0
Aslly-Hernandez <Aslly.hernandz@gmail.com>
2024-09-06 14:30:07 -0600

    Add files via upload

---
+++ GUIA05_DSP/Ejercicio1/Ejercicio1/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace Ejercicio1
+{
+    partial class frmRegistoEmpleados
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dtpFechaNac = new System.Windows.Forms.DateTimePicker();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.lblfechanac = new System.Windows.Forms.Label();
+            this.Lblapellidos = new System.Windows.Forms.Label();
+            this.Lblnombres = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.nudISSS = new System.Windows.Forms.NumericUpDown();
+            this.nudRenta = new System.Windows.Forms.NumericUpDown();
+            this.lblISSS = new System.Windows.Forms.Label();
+            this.txtSueldoini = new System.Windows.Forms.TextBox();
+            this.dtpFechaContrato = new System.Windows.Forms.DateTimePicker();
+            this.lblRenta = new System.Windows.Forms.Label();
+            this.lblSueldo = new System.Windows.Forms.Label();
+            this.lblContrato = new System.Windows.Forms.Label();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnAbrirPlanilla = new System.Windows.Forms.Button();
+            this.btnGenerarPlanilla = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudISSS)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRenta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dtpFechaNac);
+            this.groupBox1.Controls.Add(this.txtApellidos);
+            this.groupBox1.Controls.Add(this.txtNombres);
+            this.groupBox1.Controls.Add(this.lblfechanac);
+            this.groupBox1.Controls.Add(this.Lblapellidos);
+            this.groupBox1.Controls.Add(this.Lblnombres);
+            this.groupBox1.Location = new System.Drawing.Point(75, 53);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(429, 170);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos personales de empleados";
+            // 
+            // dtpFechaNac
+            // 
+            this.dtpFechaNac.Location = new System.Drawing.Point(161, 127);
+            this.dtpFechaNac.Name = "dtpFechaNac";
+            this.dtpFechaNac.Size = new System.Drawing.Size(247, 22);
+            this.dtpFechaNac.TabIndex = 5;
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(172, 77);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(157, 22);
+            this.txtApellidos.TabIndex = 4;
+            this.txtApellidos.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtApellidos_KeyPress);
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(172, 37);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(157, 22);
+            this.txtNombres.TabIndex = 3;
+            this.txtNombres.TextChanged += new System.EventHandler(this.txtNombres_TextChanged);
+            this.txtNombres.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombres_KeyPress);
+            // 
+            // lblfechanac
+            // 
+            this.lblfechanac.AutoSize = true;
+            this.lblfechanac.Location = new System.Drawing.Point(17, 133);
+            this.lblfechanac.Name = "lblfechanac";
+            this.lblfechanac.Size = new System.Drawing.Size(116, 16);
+            this.lblfechanac.TabIndex = 2;
+            this.lblfechanac.Text = "Fecha nacimiento:";
+            // 
+            // Lblapellidos
+            // 
+            this.Lblapellidos.AutoSize = true;
+            this.Lblapellidos.Location = new System.Drawing.Point(17, 83);
+            this.Lblapellidos.Name = "Lblapellidos";
+            this.Lblapellidos.Size = new System.Drawing.Size(67, 16);
+            this.Lblapellidos.TabIndex = 1;
+            this.Lblapellidos.Text = "Apellidos:";
+            // 
+            // Lblnombres
+            // 
+            this.Lblnombres.AutoSize = true;
+            this.Lblnombres.Location = new System.Drawing.Point(17, 37);
+            this.Lblnombres.Name = "Lblnombres";
+            this.Lblnombres.Size = new System.Drawing.Size(66, 16);
+            this.Lblnombres.TabIndex = 0;
+            this.Lblnombres.Text = "Nombres:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.nudISSS);
+            this.groupBox2.Controls.Add(this.nudRenta);
+            this.groupBox2.Controls.Add(this.lblISSS);
+            this.groupBox2.Controls.Add(this.txtSueldoini);
+            this.groupBox2.Controls.Add(this.dtpFechaContrato);
+            this.groupBox2.Controls.Add(this.lblRenta);
+            this.groupBox2.Controls.Add(this.lblSueldo);
+            this.groupBox2.Controls.Add(this.lblContrato);
+            this.groupBox2.Location = new System.Drawing.Point(522, 53);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(574, 170);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Datos laborales:";
+            // 
+            // nudISSS
+            // 
+            this.nudISSS.DecimalPlaces = 1;
+            this.nudISSS.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nudISSS.Location = new System.Drawing.Point(341, 131);
+            this.nudISSS.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.nudISSS.Name = "nudISSS";
+            this.nudISSS.Size = new System.Drawing.Size(120, 22);
+            this.nudISSS.TabIndex = 12;
+            this.nudISSS.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.nudISSS_KeyPress);
+            // 
+            // nudRenta
+            // 
+            this.nudRenta.DecimalPlaces = 1;
+            this.nudRenta.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nudRenta.Location = new System.Drawing.Point(114, 131);
+            this.nudRenta.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.nudRenta.Name = "nudRenta";
+            this.nudRenta.Size = new System.Drawing.Size(120, 22);
+            this.nudRenta.TabIndex = 11;
+            this.nudRenta.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.nudRenta_KeyPress);
+            // 
+            // lblISSS
+            // 
+            this.lblISSS.AutoSize = true;
+            this.lblISSS.Location = new System.Drawing.Point(262, 135);
+            this.lblISSS.Name = "lblISSS";
+            this.lblISSS.Size = new System.Drawing.Size(75, 16);
+            this.lblISSS.TabIndex = 10;
+            this.lblISSS.Text = "Tasa ISSS:";
+            // 
+            // txtSueldoini
+            // 
+            this.txtSueldoini.Location = new System.Drawing.Point(141, 83);
+            this.txtSueldoini.Name = "txtSueldoini";
+            this.txtSueldoini.Size = new System.Drawing.Size(83, 22);
+            this.txtSueldoini.TabIndex = 6;
+            this.txtSueldoini.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSueldoini_KeyPress);
+            // 
+            // dtpFechaContrato
+            // 
+            this.dtpFechaContrato.Location = new System.Drawing.Point(155, 32);
+            this.dtpFechaContrato.Name = "dtpFechaContrato";
+            this.dtpFechaContrato.Size = new System.Drawing.Size(247, 22);
+            this.dtpFechaContrato.TabIndex = 9;
+            // 
+            // lblRenta
+            // 
+            this.lblRenta.AutoSize = true;
+            this.lblRenta.Location = new System.Drawing.Point(27, 133);
+            this.lblRenta.Name = "lblRenta";
+            this.lblRenta.Size = new System.Drawing.Size(81, 16);
+            this.lblRenta.TabIndex = 8;
+            this.lblRenta.Text = "Tasa Renta:";
+            // 
+            // lblSueldo
+            // 
+            this.lblSueldo.AutoSize = true;
+            this.lblSueldo.Location = new System.Drawing.Point(27, 83);
+            this.lblSueldo.Name = "lblSueldo";
+            this.lblSueldo.Size = new System.Drawing.Size(87, 16);
+            this.lblSueldo.TabIndex = 7;
+            this.lblSueldo.Text = "Sueldo base:";
+            // 
+            // lblContrato
+            // 
+            this.lblContrato.AutoSize = true;
+            this.lblContrato.Location = new System.Drawing.Point(27, 37);
+            this.lblContrato.Name = "lblContrato";
+            this.lblContrato.Size = new System.Drawing.Size(99, 16);
+            this.lblContrato.TabIndex = 6;
+            this.lblContrato.Text = "Fecha contrato:";
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(75, 234);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(163, 63);
+            this.btnNuevo.TabIndex = 2;
+            this.btnNuevo.Text = "Nuevo empleado";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(283, 234);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(161, 63);
+            this.btnAceptar.TabIndex = 3;
+            this.btnAceptar.Text = "Proximo empleado";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnAbrirPlanilla
+            // 
+            this.btnAbrirPlanilla.Location = new System.Drawing.Point(622, 234);
+            this.btnAbrirPlanilla.Name = "btnAbrirPlanilla";
+            this.btnAbrirPlanilla.Size = new System.Drawing.Size(154, 63);
+            this.btnAbrirPlanilla.TabIndex = 4;
+            this.btnAbrirPlanilla.Text = "Abrir Planilla";
+            this.btnAbrirPlanilla.UseVisualStyleBackColor = true;
+            this.btnAbrirPlanilla.Click += new System.EventHandler(this.btnAbrirPlanilla_Click);
+            // 
+            // btnGenerarPlanilla
+            // 
+            this.btnGenerarPlanilla.Location = new System.Drawing.Point(874, 234);
+            this.btnGenerarPlanilla.Name = "btnGenerarPlanilla";
+            this.btnGenerarPlanilla.Size = new System.Drawing.Size(146, 63);
+            this.btnGenerarPlanilla.TabIndex = 5;
+            this.btnGenerarPlanilla.Text = "Generar Planilla";
+            this.btnGenerarPlanilla.UseVisualStyleBackColor = true;
+            this.btnGenerarPlanilla.Click += new System.EventHandler(this.btnGenerarPlanilla_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(75, 314);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1021, 213);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // frmRegistoEmpleados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1270, 572);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnGenerarPlanilla);
+            this.Controls.Add(this.btnAbrirPlanilla);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmRegistoEmpleados";
+            this.Text = "Registro inicial de empleados";
+            this.Load += new System.EventHandler(this.frmRegistoEmpleados_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudISSS)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRenta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.Label lblfechanac;
+        private System.Windows.Forms.Label Lblapellidos;
+        private System.Windows.Forms.Label Lblnombres;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DateTimePicker dtpFechaNac;
+        private System.Windows.Forms.NumericUpDown nudISSS;
+        private System.Windows.Forms.NumericUpDown nudRenta;
+        private System.Windows.Forms.Label lblISSS;
+        private System.Windows.Forms.TextBox txtSueldoini;
+        private System.Windows.Forms.DateTimePicker dtpFechaContrato;
+        private System.Windows.Forms.Label lblRenta;
+        private System.Windows.Forms.Label lblSueldo;
+        private System.Windows.Forms.Label lblContrato;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnAbrirPlanilla;
+        private System.Windows.Forms.Button btnGenerarPlanilla;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
