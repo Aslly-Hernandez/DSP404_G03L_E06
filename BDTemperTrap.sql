d3627bc67c3b71831c7046c811f5fe8651230ba1
PaolaC4lles <144408683+PaolaC4lles@users.noreply.github.com>
2024-09-23 02:10:31 +0200

    actualización de base de datos

--- BDTemperTrap.sql
+++ BDTemperTrap.sql
@@ -1,18 +1,18 @@
-CREATE DATABASE TemperTrap
+CREATE DATABASE TheTemperTrap
 
-USE TemperTrap
+USE TheTemperTrap
 
-CREATE TABLE Empleados(
-idEmpleado INT PRIMARY KEY NOT NULL,
+CREATE TABLE Usuarios(
+idUsuario INT PRIMARY KEY,
 Nombres VARCHAR(50),
 Correo VARCHAR(70),
 Contra VARCHAR(100),
 Direccion VARCHAR(100),
 Telefono VARCHAR(50),
-idTipoEmpleado INT NOT NULL);
+idTipoUsuario INT);
 
 CREATE TABLE Comidas(
-idComida INT PRIMARY KEY NOT NULL,
+idComida INT PRIMARY KEY,
 Nombre VARCHAR(50),
 Precio DECIMAL(10,2),
 Descripcion VARCHAR(100),
@@ -20,43 +20,43 @@ Imagen image,
 idTipoComida INT);
 
 CREATE TABLE TipoComidas(
-idTipoComida INT PRIMARY KEY NOT NULL,
+idTipoComida INT PRIMARY KEY,
 Descripcion VARCHAR(60));
 
-CREATE TABLE TipoEmpleado(
-idTipoEmpleado INT PRIMARY KEY NOT NULL,
+CREATE TABLE TipoUsuario(
+idTipoUsuario INT PRIMARY KEY,
 Descripcion VARCHAR(60));
 
 CREATE TABLE Pedido(
-idPedido INT PRIMARY KEY NOT NULL,
+idPedido INT PRIMARY KEY,
 Estado VARCHAR(50),
 Comentario VARCHAR(100),
-idEmpleado INT);
+idUsuario INT);
 
 CREATE TABLE Pedido_Comidas(
-idPedido_Comidas INT PRIMARY KEY NOT NULL,
+idPedido_Comidas INT PRIMARY KEY,
 Comentario VARCHAR(100),
 idPedido INT,
 idComida INT);
 
 CREATE TABLE ModoPago(
-idModoPago INT PRIMARY KEY NOT NULL,
+idModoPago INT PRIMARY KEY,
 Descripcion VARCHAR(100));
 
 CREATE TABLE DetalleCarrito(
-idDetalle INT PRIMARY KEY NOT NULL,
+idDetalle INT PRIMARY KEY,
 PrecioFinal DECIMAL(10,2),
 idModoPago INT,
 idPedido INT);
 
-ALTER TABLE Empleados
-ADD CONSTRAINT FK_Empleados_TipoEmpleado FOREIGN KEY (idTipoEmpleado) REFERENCES TipoEmpleado(idTipoEmpleado);
+ALTER TABLE Usuarios
+ADD CONSTRAINT FK_Usuarios_TipoUsuario FOREIGN KEY (idTipoUsuario) REFERENCES TipoUsuario(idTipoUsuario);
 
 ALTER TABLE Comidas
 ADD CONSTRAINT FK_Comidas_TipoComidas FOREIGN KEY (idTipoComida) REFERENCES TipoComidas(idTipoComida);
 
 ALTER TABLE Pedido
-ADD CONSTRAINT FK_Pedido_Empleados FOREIGN KEY (idEmpleado) REFERENCES Empleados(idEmpleado);
+ADD CONSTRAINT FK_Pedido_Usuarios FOREIGN KEY (idUsuario) REFERENCES Usuarios(idUsuario);
 
 ALTER TABLE Pedido_Comidas
 ADD CONSTRAINT FK_Pedido_Pedido_Comidas FOREIGN KEY (idPedido) REFERENCES Pedido(idPedido);
@@ -69,3 +69,45 @@ ADD CONSTRAINT FK_DetalleCarrito_ModoPago FOREIGN KEY (idModoPago) REFERENCES Mo
 
 ALTER TABLE DetalleCarrito
 ADD CONSTRAINT FK_DetalleCarrito_Pedido FOREIGN KEY (idPedido) REFERENCES Pedido(idPedido);
+
+
+CREATE PROC sp_RegistrarUsuario(
+@Nombres VARCHAR(50),
+@Correo VARCHAR(70),
+@Contra VARCHAR(100),
+@Direccion VARCHAR(100),
+@Telefono VARCHAR(50),
+@Registrado BIT OUTPUT,
+@Mensaje VARCHAR(100) OUTPUT
+)
+AS
+BEGIN
+
+IF(NOT EXISTS (SELECT * FROM Usuarios WHERE Correo = @Correo))
+BEGIN
+INSERT INTO Usuarios(Correo,Contra) VALUES (@Correo,@Contra)
+SET @Registrado = 1
+SET @Mensaje = 'Usuario Registrado'
+END
+ELSE
+BEGIN
+SET @Registrado = 0
+SET @Mensaje = 'Correo existente'
+END
+END
+
+CREATE PROC sp_ValidarUsuario(
+@Correo VARCHAR(70),
+@Contra VARCHAR(100))
+AS
+BEGIN
+IF (EXISTS(SELECT * FROM Usuarios WHERE Correo = @Correo AND Contra = @Contra))
+SELECT idUsuario FROM Usuarios WHERE Correo = @Correo AND Contra = @Contra
+ELSE
+SELECT '0'
+END
+
+SELECT * FROM USUARIOS
+
+
+
