9905883bf881c9fa1ca1e998ee9969d7c313384c
PaolaC4lles <144408683+PaolaC4lles@users.noreply.github.com>
2024-09-24 04:06:46 +0200

    guia07

---
+++ CajeroWeb/CajeroWeb/Models/Transaccion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CajeroWeb.Models
+{
+    public class Transaccion
+    {
+        private string sNumeroTarjeta;
+        private double fSaldo;
+
+        public Transaccion(string tarjeta)
+        {
+            sNumeroTarjeta = tarjeta;
+            fSaldo = 500;
+        }
+
+        public Transaccion(string tarjeta, double saldo)
+        {
+            sNumeroTarjeta = tarjeta;
+            fSaldo = saldo;
+        }
+        public double getSaldo()
+        {
+            return fSaldo;
+        }
+        public void setSaldo(double saldo)
+        {
+            fSaldo = saldo;
+        }
+        public String getNumTarjeta()
+        {
+            return sNumeroTarjeta;
+        }
+        public void setNumTarjeta(string tarjeta)
+        {
+            sNumeroTarjeta = tarjeta;
+        }
+        public void transferenciaNacional(double monto)
+        {
+            fSaldo = fSaldo - monto;
+        }
+        public void transferenciaInternacional(double monto)
+        {
+            fSaldo = fSaldo - monto;
+        }
+    }
+}
